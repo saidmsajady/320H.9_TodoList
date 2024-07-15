e0f6dc332f6d2d28f12054532e7919b5fb57a28a
Said Masih <saidmsajady@gmail.com>
2024-07-14 19:39:53 -0700

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import todoData from './utilities/data';
 export const actions = {
   add_todo: 'add-todo',
   toggle_todo: 'toggle-todo',
-  delete_todo: 'delete-todo'
+  delete_todo: 'delete-todo',
+  edit_todo: 'edit-todo',
+  save_todo: 'save-todo'
 };
 
 function reducer(todos, action) {
   switch (action.type) {
     case actions.add_todo:
-      return [...todos, newTodo(action.payload.name)];
+      return [{ ...newTodo(action.payload.name) }, ...todos];
     case actions.toggle_todo:
       return todos.map(todo => {
         if (todo.id === action.payload.id) {
@@ -22,30 +24,47 @@ function reducer(todos, action) {
       });
     case actions.delete_todo:
       return todos.filter(todo => todo.id !== action.payload.id);
+    case actions.edit_todo:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, isEditing: true };
+        }
+        return todo;
+      });
+    case actions.save_todo:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, name: action.payload.name, isEditing: false };
+        }
+        return todo;
+      });
     default:
       return todos;
   }
 }
 
 function newTodo(name) {
-  return { id: Date.now(), name: name, complete: false };
+  return { id: Date.now() + Math.random(), name: name, complete: false, isEditing: false }; // Ensure unique ID by adding Math.random()
 }
 
 function App() {
-  const [todos, dispatch] = useReducer(reducer, todoData);
+  const [todos, dispatch] = useReducer(reducer, todoData.map(name => newTodo(name)));
   const [name, setName] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
-    dispatch({ type: actions.add_todo, payload: { name: name } });
-    setName('');
+    if (name.trim()) {
+      dispatch({ type: actions.add_todo, payload: { name: name } });
+      setName('');
+    }
   }
 
   return (
     <>
       <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        <input type="text" value={name} placeholder="Enter New Todo Here" onChange={e => setName(e.target.value)} />
+        <button type="submit" >Add Todo</button>
       </form>
       {todos.map(todo => (
         <Todo key={todo.id} todo={todo} dispatch={dispatch} />
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { actions } from '../App';
 
 const Todo = ({ todo, dispatch }) => {
+  const [editName, setEditName] = useState(todo.name);
+
+  const handleSave = () => {
+    dispatch({ type: actions.save_todo, payload: { id: todo.id, name: editName } });
+  };
+
   return (
     <div>
-      <span style={{ color: todo.complete ? '#AAA' : '#000' }}>
-        {todo.name}
-      </span>
-      <button onClick={() => dispatch({ type: actions.toggle_todo, payload: { id: todo.id } })}>
-        Toggle
-      </button>
-      <button onClick={() => dispatch({ type: actions.delete_todo, payload: { id: todo.id } })}>
-        Delete
-      </button>
+      <input
+        type="checkbox"
+        checked={todo.complete}
+        onChange={() => dispatch({ type: actions.toggle_todo, payload: { id: todo.id } })}
+      />
+      {todo.isEditing ? (
+        <>
+          <input
+            type="text"
+            value={editName}
+            onChange={e => setEditName(e.target.value)}
+          />
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <>
+          <span style={{ color: todo.complete ? '#AAA' : '#000' }}>
+            {todo.name}
+          </span>
+          <button onClick={() => dispatch({ type: actions.edit_todo, payload: { id: todo.id } })}>
+            Edit
+          </button>
+          <button
+            onClick={() => dispatch({ type: actions.delete_todo, payload: { id: todo.id } })}
+            disabled={!todo.complete}>Delete</button>
+        </>
+      )}
     </div>
   );
 };
