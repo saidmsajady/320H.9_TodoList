086a1df25256171f0c72413ee903276e35ec0bd4
Said Masih <saidmsajady@gmail.com>
2024-07-14 19:33:55 -0700

    Baic Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React, { useReducer, useState } from 'react';
 import './App.css';
 import Todo from './components/Todo';
@@ -7,15 +6,13 @@ import todoData from './utilities/data';
 export const actions = {
   add_todo: 'add-todo',
   toggle_todo: 'toggle-todo',
-  delete_todo: 'delete-todo',
-  edit_todo: 'edit-todo',
-  save_todo: 'save-todo'
+  delete_todo: 'delete-todo'
 };
 
 function reducer(todos, action) {
   switch (action.type) {
     case actions.add_todo:
-      return [{ ...newTodo(action.payload.name) }, ...todos];
+      return [...todos, newTodo(action.payload.name)];
     case actions.toggle_todo:
       return todos.map(todo => {
         if (todo.id === action.payload.id) {
@@ -25,39 +22,23 @@ function reducer(todos, action) {
       });
     case actions.delete_todo:
       return todos.filter(todo => todo.id !== action.payload.id);
-    case actions.edit_todo:
-      return todos.map(todo => {
-        if (todo.id === action.payload.id) {
-          return { ...todo, isEditing: true };
-        }
-        return todo;
-      });
-    case actions.save_todo:
-      return todos.map(todo => {
-        if (todo.id === action.payload.id) {
-          return { ...todo, name: action.payload.name, isEditing: false };
-        }
-        return todo;
-      });
     default:
       return todos;
   }
 }
 
 function newTodo(name) {
-  return { id: Date.now(), name: name, complete: false, isEditing: false };
+  return { id: Date.now(), name: name, complete: false };
 }
 
 function App() {
-  const [todos, dispatch] = useReducer(reducer, todoData.map(name => newTodo(name)));
+  const [todos, dispatch] = useReducer(reducer, todoData);
   const [name, setName] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (name.trim()) {
-      dispatch({ type: actions.add_todo, payload: { name: name } });
-      setName('');
-    }
+    dispatch({ type: actions.add_todo, payload: { name: name } });
+    setName('');
   }
 
   return (
@@ -65,7 +46,6 @@ function App() {
       <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" value={name} onChange={e => setName(e.target.value)} />
-        <button type="submit">Add Todo</button>
       </form>
       {todos.map(todo => (
         <Todo key={todo.id} todo={todo} dispatch={dispatch} />
